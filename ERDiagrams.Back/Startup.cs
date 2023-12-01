5608961b9ed005bcffc80c6925aee52ae8e1a796
TempooDev <a.bermudez.rodriguez@uma.es>
2023-12-01 19:04:21 +0100

    FIx nugget packages

--- ERDiagrams.Back/Startup.cs
+++ ERDiagrams.Back/Startup.cs
@@ -1,36 +1,31 @@
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using System;
 using ERDiagrams.Back;
-using ERDiagrams.Back.Models;
-using ERDiagrams.Back.Repositories;
-using ERDiagrams.Back.Repositories.Interfaces;
-using ERDiagrams.Back.Services;
-using ERDiagrams.Back.Services.Interfaces;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-[assembly : FunctionsStartup(typeof(Startup))]
+[assembly: FunctionsStartup(typeof(Startup))]
 
-namespace ERDiagrams.Back;
-public class Startup : FunctionsStartup
+namespace ERDiagrams.Back
 {
-    public override void Configure(IFunctionsHostBuilder builder)
+    public class Startup : FunctionsStartup
     {
-        ConfigureServices(builder.Services).BuildServiceProvider(true);
-    }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            ConfigureServices(builder.Services).BuildServiceProvider(true);
+        }
 
-    private IServiceCollection ConfigureServices(IServiceCollection services)
-    {
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Environment.CurrentDirectory)
-            .AddJsonFile("local.settings.json", true, true)
-            .AddEnvironmentVariables()
-            .Build();
+        private IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
 
-        services.AddSingleton(new FunctionConfiguration(config));
-        services.AddScoped<IRepository<Book>, BookRepository>();
-        services.AddScoped<IBookService, BookService>();
-        
-        return services;
+            services.AddSingleton(new FunctionConfiguration(config));
+
+            return services;
+        }
     }
 }
