68ed75a37327c6c7fb8e086d7e7f0792807899e6
TempooDev <a.bermudez.rodriguez@uma.es>
2024-07-07 18:58:54 +0200

    feat: name regions

--- ERDiagrams.Collaborative/Program.cs
+++ ERDiagrams.Collaborative/Program.cs
@@ -11,17 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+#region CONFIG
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var  connectionString = builder.Configuration.GetConnectionString("MongoDB");
+var connectionString = builder.Configuration.GetConnectionString("MongoDB");
 // Add services to the container.
 builder.Services.AddSingleton<IMongoClient, MongoClient>(_ =>
     new MongoClient(connectionString));
 
-builder.Services.AddControllers()
-    .AddJsonOptions(
-        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+#endregion
+
+#region CORS POLICY
+
 string[] allowedOrigins =
 [
     "http://localhost:3000",
@@ -36,14 +38,27 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader()
             .AllowCredentials()); // Importante para SignalR
 });
+
+#endregion
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+#region SERVICES
+
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
+
+#endregion
+
 var config = new MapperConfiguration(cfg => cfg.CreateMap<Diagram, DiagramDto>());
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors("AllowSpecificOrigin");
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
@@ -52,27 +67,26 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+#region API DIAGRAMS
+
 app.MapGet("/diagrams", ([FromServices] IMongoClient mongoClient) =>
     {
         var mapper = config.CreateMapper();
         var database = DiagramDbContext.Create(mongoClient.GetDatabase("ERDiagram"));
-        var diagrams =database.Diagrams.ToArrayAsync();
+        var diagrams = database.Diagrams.ToArrayAsync();
         Task.WaitAll();
         return mapper.Map<Diagram[], DiagramDto[]>(diagrams.Result);
-
     })
     .WithName("GetAllDiagrams")
     .WithOpenApi();
 
 app.MapGet("/diagrams/{diagramId}", DiagramDto ([FromServices] IMongoClient mongoClient, string diagramId) =>
     {
-        
         var mapper = config.CreateMapper();
         var database = DiagramDbContext.Create(mongoClient.GetDatabase("ERDiagram"));
-        var diagram =  database.Diagrams.FirstOrDefaultAsync(diagram => diagram.diagramId == diagramId);
+        var diagram = database.Diagrams.FirstOrDefaultAsync(diagram => diagram.diagramId == diagramId);
         Task.WaitAll();
         return mapper.Map<Diagram, DiagramDto>(diagram.Result);
-        
     }).WithName("GetDiagramById")
     .WithOpenApi();
 
@@ -94,20 +108,20 @@ app.MapPost("/diagrams", async ([FromServices] IMongoClient mongoClient, Diagram
     }).WithName("PostDiagram")
     .WithOpenApi();
 
-app.MapPut("/diagrams/{diagramId}", async ([FromServices] IMongoClient mongoClient, string diagramId, DiagramDto diagramToUpdate) =>
-    {
-        var database = DiagramDbContext.Create(mongoClient.GetDatabase("ERDiagram"));
-        var diagram =  database.Diagrams.FirstOrDefaultAsync(dia => dia.diagramId.Equals( diagramId));
-        Task.WaitAll();
-        if (diagram.Result is null) return Results.NotFound();
-        diagram.Result.image = diagramToUpdate.image;
-        diagram.Result.name = diagramToUpdate.name;
-        diagram.Result.linkDataArray = diagramToUpdate.linkDataArray;
-        diagram.Result.nodeDataArray = diagramToUpdate.nodeDataArray;
-        await database.SaveChangesAsync();
-        return Results.NoContent();
-
-    }).WithName("UpdateDiagram")
+app.MapPut("/diagrams/{diagramId}",
+        async ([FromServices] IMongoClient mongoClient, string diagramId, DiagramDto diagramToUpdate) =>
+        {
+            var database = DiagramDbContext.Create(mongoClient.GetDatabase("ERDiagram"));
+            var diagram = database.Diagrams.FirstOrDefaultAsync(dia => dia.diagramId.Equals(diagramId));
+            Task.WaitAll();
+            if (diagram.Result is null) return Results.NotFound();
+            diagram.Result.image = diagramToUpdate.image;
+            diagram.Result.name = diagramToUpdate.name;
+            diagram.Result.linkDataArray = diagramToUpdate.linkDataArray;
+            diagram.Result.nodeDataArray = diagramToUpdate.nodeDataArray;
+            await database.SaveChangesAsync();
+            return Results.NoContent();
+        }).WithName("UpdateDiagram")
     .WithOpenApi();
 
 app.MapDelete("/diagrams/{diagramId}", async ([FromServices] IMongoClient mongoClient, string diagramId) =>
@@ -122,12 +136,17 @@ app.MapDelete("/diagrams/{diagramId}", async ([FromServices] IMongoClient mongoC
 
     return Results.NotFound();
 });
-app.UseRouting();
+
+#endregion
+
+#region HUB
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapHub<BoardHub>("/hub/board");
-    // Otros mapeos de endpoints
 });
 
+#endregion
+
+app.UseRouting();
 app.Run();
