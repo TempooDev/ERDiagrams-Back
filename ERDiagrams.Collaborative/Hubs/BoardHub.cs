639fce29622133fcdea413b32d029a85913d250a
TempooDev <a.bermudez.rodriguez@uma.es>
2024-07-07 20:00:40 +0200

    feat: groups

--- ERDiagrams.Collaborative/Hubs/BoardHub.cs
+++ ERDiagrams.Collaborative/Hubs/BoardHub.cs
@@ -6,8 +6,19 @@ namespace ERDiagrams.Collaborative.Hubs;
 
 public class BoardHub : Hub
 {
-   
-  
+    public async Task AddToGroup(string groupName)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+    }
+
+    public async Task RemoveFromGroup(string groupName)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+        await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+    }
     public async Task Send(Diagram diagram)
     {
        
